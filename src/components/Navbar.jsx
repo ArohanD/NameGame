fed8e896dc655a9d5fea641d1e5d9fe88e835acf
Arohan Dutt <arohandutt@gmail.com>
2020-01-29 00:02:13 -0600

    Style NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Icon, InlineIcon } from '@iconify/react'
 import textSize from '@iconify/icons-fe/text-size'
+import menuIcon from '@iconify/icons-mdi/menu'
 import ThemeContext from '../ThemeContext.jsx'
 
 const Navbar = () => {
@@ -11,12 +12,32 @@ const Navbar = () => {
     height: '50px',
     width: '100%',
     position: 'fixed',
-    bottom: 0
+    bottom: 0,
+
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+
+  const iconsStyle = {
+    width: '90%',
+    height: '100%',
+    display: 'flex',
+
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  }
+
+  const iconStyle = {
+    color: '#F6F6F6'
   }
 
   return (
     <div style={navStyle}>
-      <Icon icon={textSize} style={{ color: '#F6F6F6' }} />
+      <div style={iconsStyle}>
+        <Icon icon={menuIcon} height={'2.2em'} style={iconStyle} />
+        <Icon icon={textSize} height={'2.2em'} style={iconStyle} />
+      </div>
     </div>
   )
 }
